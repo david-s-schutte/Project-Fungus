0848c611a751129003c237d6d7c585180565be35
david-s-schutte <david.schutte01@gmail.com>
2023-05-31 14:46:33 +1000

    Attempted player movement - likely to be scrapped

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float moveSpeed;
+    Rigidbody2D rb;
+    [SerializeField] private float freeWill;
+    Vector3 mousePosition;
+    Vector3 moveDirection;
+    [SerializeField] GameObject marker;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        mousePosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(mousePosition);
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (willListenToPlayer())
+            {
+                mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition.z = 0;
+                Instantiate(marker, mousePosition, Quaternion.identity);
+            }
+        }
+
+        if (shouldMovePlayer() && !isPlayerMoving())
+        {
+            moveDirection = mousePosition - transform.position;
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            rb.AddForce(moveDirection * moveSpeed);
+        }
+        else if (!shouldMovePlayer())
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private bool willListenToPlayer() {
+        float playersWill = Random.Range(0, 100);
+        return playersWill > freeWill;
+    }
+
+    private bool isPlayerMoving()
+    {
+        return (rb.velocity.x > 0 && rb.velocity.y > 0);
+    }
+
+    private bool shouldMovePlayer()
+    {
+        return ((transform.position.x < mousePosition.x || transform.position.x > mousePosition.x) && 
+            (transform.position.y < mousePosition.y) || (transform.position.y < mousePosition.y));
+    }
+
+    IEnumerator MovePlayer(float duration)
+    {
+        float time = 0;
+        Vector2 startPosition = transform.position;
+        while(time < duration)
+        {
+            rb.AddForce(moveDirection * time/duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = moveDirection;
     }
 }
